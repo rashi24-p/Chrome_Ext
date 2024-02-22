3ac668fef36ead6e00530530bdc41452569f3703
Rashi Porwal <porwalrashi786@gmail.com>
2024-02-22 21:55:19 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,42 @@
+// function sendNotification(notificationId, message) {
+//     chrome.notifications.create(notificationId, {
+//       type: "basic",
+//       title: "A user joined the call",
+//       message: message,
+//       iconUrl: "./success.png"
+//     });
+// }
+
+// sendNotification("notif-id", "test message");
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     if (message.action === "people_joined") {
+//       sendNotification("notif-id", message.txt);
+//     }
+//   });
+  
+
+// function sendNotification(notificationId, message) {
+//     chrome.notifications.create(notificationId, {
+//       type: "basic",
+//       title: "A user joined the call",
+//       message: message,
+//       iconUrl: "./success.png"
+//     });
+// }
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "people_joined") {
+      sendNotification("" + sender.tab.id, message.txt); // We are casting this to string as notificationId is expected to be a string while sender.tab.id is an integer.
+    }
+  });
+  
+
+function sendNotification(notificationId, message) {
+    chrome.notifications.create(notificationId, {
+      type: "basic",
+      title: "A user joined the call",
+      message: message,
+      iconUrl: "./success.png"
+    });
+}
